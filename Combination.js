3e35caae6d0d9a23a384fcd76a0db2ee1169cf83
KyeongHa <rudgk030@hanyang.ac.kr>
2023-03-26 19:44:32 +0900

    2023-03-26

--- Combination.js
+++ Combination.js
@@ -1,17 +1,33 @@
 /* 가능한 모든 조합 구하기 */
 function getCombinations(arr, N) {
     const results = [];
-    if (N === 1) return arr.map((value) => [value]);
+    if (N === 1) return arr.map((el) => [el]);
   
     arr.forEach((fixed, index, origin) => {
       const rest = origin.slice(index + 1);
       const combinations = getCombinations(rest, N - 1);
-      const attached = combinations.map((combination) => [fixed, ...combination]);
+      const attached = combinations.map((el) => [fixed, ...el]);
       results.push(...attached);
     });
   
     return results;
 }
+
+/** 중복조합 */
+function getRedundantCombination(arr, N){
+  const result = [];
+  if(N === 1) return arr.map((el) => [el]);
+
+  arr.forEach((fixed, index, origin) => {
+      const combination = getRedundantCombination(origin, N-1);
+      const attached = combination.map((el) => [fixed, ...el]);
+
+      result.push(...attached);
+  });
+
+  return result;
+}
+
 /*
     1을 선택(고정)하고 -> 나머지 [2, 3, 4] 중에서 2개씩 조합을 구한다.
     [1, 2, 3] [1, 2, 4] [1, 3, 4]
--- Permutations.js
+++ Permutations.js
@@ -1,39 +1,30 @@
-/* 순열 구하기 */
-/* 서로 다른 n개의 물건 중에서 r개를 택하여 한 줄로 배열하는 것 */
+/** 순열 : 서로 다른 n개의 물건 중에서 r개를 택하여 한 줄로 배열하는 것 */
 const getPermutations = function (arr, selectNumber) {
     const results = [];
     if (selectNumber === 1) return arr.map((el) => [el]); 
-    // n개중에서 1개 선택할 때(nP1), 바로 모든 배열의 원소 return. 1개선택이므로 순서가 의미없음.
 
     arr.forEach((fixed, index, origin) => {
-      const rest = [...origin.slice(0, index), ...origin.slice(index+1)] 
-      // 해당하는 fixed를 제외한 나머지 배열 
+      const rest = [...origin.slice(0, index), ...origin.slice(index+1)];
+      
       const permutations = getPermutations(rest, selectNumber - 1); 
-      // 나머지에 대해서 순열을 구한다.
+
       const attached = permutations.map((el) => [fixed, ...el]); 
-      //  돌아온 순열에 떼 놓은(fixed) 값 붙이기
       results.push(...attached); 
-      // 배열 spread syntax 로 모두다 push
     });
 
-    return results; // 결과 담긴 results return
+    return results; 
 }
 
 
-/* 중복 순열 구하기 */
-const getPermutations = (arr, num) => {
+/** 중복순열 */
+const getRedundantPermutations = (arr, num) => {
     const results = [];
     if (num === 1) return arr.map(v => [v]);
 
     arr.forEach((fixed, index, origin) => {
-        
-        // 기준값(fixed)이 있기 때문에 선택하려는 개수에서 - 1 을 해준다.
-        const permutations = getPermutations(origin, num - 1);
+        const permutations = getRedundantPermutations(origin, num - 1); // origin을 slice하지 않는다는 것이 특징
 
-        // 기준값(fixed)에 순열(permutations)을 붙인다.
-        const attached = permutations.map(v => [fixed, ...v]);
-
-        // 붙인 값을 결과 값에 넣어준다.
+        const attached = permutations.map(el => [fixed, ...el]);
         results.push(...attached);
     });
 
